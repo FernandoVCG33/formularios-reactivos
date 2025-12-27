a4333c3025678274756844dbd62114720f903c86
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-27 18:27:25 -0400

    Add country service and interface for fetching country data by region and code

---
+++ reactive-forms-app/src/app/country/services/country.service.ts
@@ -0,0 +1,38 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {Country} from '../interfaces/country.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+
+  private baseUrl='https://restcountries.com/v3.1/';
+  private http=inject(HttpClient);
+
+  private _regions=[
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania'
+  ];
+  getRegions():string[]{
+    return [...this._regions];
+  }
+  getCountriesByRegion(region:string): Observable<Country[]>{
+    if(!region) return of([]);
+
+    const url = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.http.get<Country[]>(url);
+
+  }
+  getCountryByCode(code:string):Observable<Country | null>{
+    const url = `${this.baseUrl}/alpha/${code}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url);
+  }
+  getCountriesByCodes(borders:string[]){
+
+  }
+}
