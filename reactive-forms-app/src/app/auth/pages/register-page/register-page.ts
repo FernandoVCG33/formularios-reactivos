f4b33db24c7fd2a13753ae8aae6868db9ab741fc
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-27 16:25:07 -0400

    Enhance registration form with improved validation and error handling for fields

--- reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
+++ reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
@@ -1,17 +1,28 @@
 import {Component, inject} from '@angular/core';
 import {JsonPipe} from '@angular/common';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormUtils} from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './register-page.html',
 })
 export class RegisterPage {
+  formUtils=FormUtils;
   private fb=inject(FormBuilder);
   myForm:FormGroup= this.fb.group({
-
+      name:['', Validators.required,Validators.pattern(FormUtils.namePattern)],
+      email:['', [Validators.required, Validators.pattern(FormUtils.emailPattern)]],
+      username:['', [ Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern)]],
+      password:['', [Validators.required, Validators.minLength(6)]],
+      password2:['', [Validators.required]],
   });
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+    this.myForm.value;
+  }
 }
--- reactive-forms-app/src/app/utils/form-utils.ts
+++ reactive-forms-app/src/app/utils/form-utils.ts
@@ -2,6 +2,10 @@ import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
 
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   static isValidField(form: FormGroup, fieldName: string): boolean {
     // Se ha simplificado la expresión y se ha asegurado que siempre devuelva un booleano.
     return !!(form.controls[fieldName]?.errors && form.controls[fieldName]?.touched);
